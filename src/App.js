d1969776c10bfe23b610c6dbd0c7ef358ad46085
Punam Surwase <punamsurwase2000@gmail.com>
2023-04-24 11:19:30 +0530

    Hacker-quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+// import Main from './Components/Main/Main';
 import './App.css';
-
+import binarytree from './Components/Images/binarytree.png';
+import code from './Components/Images/code.png';
+import contenders from './Components/Images/contenders.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+<>
+<div className='content'>
+   <div className='left1'>
+      <div className='content1'>
+        <h4>Leave the binary tree behind</h4>
+        <p>Build coding questions using our library of dev-friendly content that challenges them to solve the problems yhey'd actually tackle on the job</p>
+        <a href="/get-started/hire" target="_blank">
+           Start free trial -
+        </a>    
+      </div>
+      <div className='img1'>
+        <img src={binarytree} alt="Logo"/> 
+      </div>
+   </div>        
+
+   <div className='left2'>      
+      <div className='content2'>
+        <h4>Focus on what really matters: the code.</h4>
+        <p>Build skills-based coding tests with tech problems a developer would encounter on the job - and hire the team behind your next big idea </p>
+        <a href="/get-started/hire" target="_blank">
+           Request a demo -
         </a>
-      </header>
-    </div>
+      </div>
+      <div className='img2'>
+         <img src={code} alt="Logo" />
+      </div>
+   </div>
+
+   <div className='left3'>
+      <div className='content3'>
+        <h4>Leave the binary tree behind.</h4>
+        <p>Build coding questions using our library of dev-friendly content that challenges them to solve the problems yhey'd actually tackle on the job</p>
+        <a href="/get-started/hire" target="_blank">
+           Learn more -
+        </a>
+      </div> 
+      <div className='img3'>
+        <img src={contenders} alt="Logo" />
+      </div>
+   </div>
+</div>
+ </> 
   );
 }
 
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,42 @@
+import React from "react";
+// import { ReactDOM } from "react";
+
+
+const Main =() => {
+   <>
+    return 
+    (    
+      
+        <div className='content1'>
+        <h4>Leave the binary tree behind.</h4>
+        <p>Build coding questions using our library of dev-friendly content that challenges them to solve the problems yhey'd actually tackle on the job</p>
+        <a href="/get-started/hire" target="_blank">
+           Start free trial -
+        </a>
+        <a href="https://www.hackerrank.com/wp-content/uploads/2022/11/binarytree-1.png"> </a>
+        </div>
+        
+        <div className='content2'>
+        <h4>Focus on what really matters: the code.</h4>
+        <p>Build skills-based coding tests with tech problems a developer would encounter on the job - and hire the team behind your next big idea </p>
+        <a href="/get-started/hire" target="_blank">
+           Request a demo -
+        </a>
+        <a href="https://www.hackerrank.com/wp-content/uploads/2022/11/code-1.png"> </a>
+        </div>
+
+        <div className='content3'>
+        <h4>Leave the binary tree behind.</h4>
+        <p>Build coding questions using our library of dev-friendly content that challenges them to solve the problems yhey'd actually tackle on the job</p>
+        <a href="/get-started/hire" target="_blank">
+           Learn more -
+        </a>
+        <a href="https://www.hackerrank.com/wp-content/uploads/2022/09/contenders.png"> </a>
+        </div> 
+    )
+    </>
+}
+
+export default Main;
+
+
